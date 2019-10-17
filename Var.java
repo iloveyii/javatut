54c0167a9479e6e1e6a9fe9751a5c48756c5af2c
Hazrat Ali <iloveyii@yahoo.com>
2019-10-17 13:26:12 +0200

    Added variables

---
+++ Var.java
@@ -0,0 +1,27 @@
+public class Var {
+
+    public static void main(String [] args) {
+        System.out.println("Hello world !");
+
+        // VARIABLES
+        String name = "Alex";
+        char gradde = 'A';
+
+        byte age1 = 10;
+        short age2 = 20;
+        int age3 = 30;
+
+        double gpa1 = 3.5;
+        float gpa2 = 2.1f;
+        long age4 = 30L;
+
+        boolean isTall;
+        isTall = true;
+
+        System.out.printf("Your name is %s", name);
+        System.out.println("");
+        System.out.println("Your name is " + name);
+
+
+    }
+}
