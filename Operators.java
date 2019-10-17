4489319348d250978f27b352c4771a7736b9b768
Hazrat Ali <iloveyii@yahoo.com>
2019-10-17 13:40:33 +0200

    Added operators

---
+++ Operators.java
@@ -0,0 +1,31 @@
+
+
+public class Operators {
+
+    public static void main(String[] args) {
+
+
+        // ARITHMETIC OPERATORS
+
+        System.out.println(2 + 3);
+        System.out.println(2 * 3);
+
+        System.out.println((1 + 2) * 3);
+
+        System.out.println(10 / 3.0); // Doubles operations returns double result
+        System.out.println(10 / 3); // Integer operations returns integer result
+        System.out.println( (double)10 / 3); // Typecast operations returns type cast result
+
+
+        // COMPARISON OPERATORS
+        System.out.println( 2 > 3);
+
+        // LOGICAL OPERATORS
+        boolean isTall = true;
+        boolean smart = true;
+        System.out.println( isTall && smart);
+
+    }
+
+
+}
